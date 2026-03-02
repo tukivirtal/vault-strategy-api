007dad47290727af316633514ac3c8f6f2189003
tukivirtal <fcippollini@gmail.com>
2026-03-01 21:45:23 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-win.loadFile('boveda.html');
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 900,
+    backgroundColor: '#050505',
+    autoHideMenuBar: true, // Esto quita los menús de "archivo" aburridos
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadFile('boveda.html'); // Aquí es donde cargamos tu joya
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
