cf3e5bce4a51a99fa73bcd8f1fed32ff237a3423
tukivirtal <fcippollini@gmail.com>
2026-03-21 19:48:48 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  base: './', 
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '');
+  return {
+    root: './', // <--- Añade esto para forzar la raíz
+    base: './',
+    plugins: [react(), tailwindcss()],
+    // ... resto del archivo igual
+  };
 });
